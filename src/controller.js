d7de2f06a238fc63d7637c4c0a4c203af3040103
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-15 11:52:01 +0100

    Add tip of the day

--- src/controller.js
+++ src/controller.js
@@ -1,9 +1,21 @@
 module.exports = ({ maxdome }) => ['get', ['/', async (req, res) => {
+  const tipOfTheDay = (await maxdome.request('tipofthedays').send())[0];
+  if (!tipOfTheDay) {
+    res.send();
+    return;
+  }
+  const review = tipOfTheDay.review;
+  if (!review) {
+    res.send();
+    return;
+  }
+  const asset = review.asset;
+  const maxpert = review.maxpert;
   res.send({
-    uid: 'uid',
-    updateDate: 'updateDate',
-    titleText: 'titleText',
-    mainText: 'mainText',
-    redirectionUrl: 'redirectionUrl',
+    uid: asset.link,
+    updateDate: tipOfTheDay.published.toISOString(),
+    titleText: `Der Tipp des Tages von ${maxpert.firstname} ${maxpert.surname}: ${asset.title}, ${review.headline}`,
+    mainText: asset.description,
+    redirectionUrl: asset.link,
   });
 }]];
