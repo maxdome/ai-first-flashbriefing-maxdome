5271c317ea7a4a8a16550accc8029804556ad256
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-03-20 11:15:49 +0100

    Add "Der Film ..."/"Die Serie ..." to the title

--- src/controller.js
+++ src/controller.js
@@ -10,11 +10,20 @@ module.exports = ({ maxdome }) => ['get', ['/', async (req, res) => {
     return;
   }
   const asset = review.asset;
+  let enhancedTitle = asset.title;
+  switch (asset.type) {
+    case 'movie':
+      enhancedTitle = `Der Film ${asset.title}`;
+      break;
+    case 'series':
+      enhancedTitle = `Die Serie ${asset.title}`;
+      break;
+  }
   const maxpert = review.maxpert;
   res.send({
     uid: asset.link,
     updateDate: tipOfTheDay.published.toISOString(),
-    titleText: `Der Tipp des Tages von ${maxpert.firstname} ${maxpert.surname}: ${asset.title}, ${review.headline}`,
+    titleText: `Der Tipp des Tages von ${maxpert.firstname} ${maxpert.surname}: ${enhancedTitle}, ${review.headline}`,
     mainText: asset.description,
     redirectionUrl: asset.link,
   });
