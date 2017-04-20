0e6872be4f5f61658f0b36c893d633b75d79979f
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-20 13:39:36 +0200

    Remove the client IP as x-forwarded-for

--- src/web.js
+++ src/web.js
@@ -1,7 +1,6 @@
 require('dotenv').config({ silent: true });
 
 const { app } = require('dexpress');
-app.enable('trust proxy');
 const maxdome = require('drequest-maxdome').getRequestBuilder();
 require('dcontrollers')(app, [
   require('./controller')({ maxdome }),
