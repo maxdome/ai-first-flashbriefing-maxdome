5522d85d13a2a15ea75690f781839f5d6ebbc968
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-04-28 12:23:53 +0200

    Use 'ai-renderer-maxdome'

--- src/controller.js
+++ src/controller.js
@@ -1,25 +1,15 @@
+const renderer = require('ai-renderer-maxdome');
+
 module.exports = ({ maxdome }) => ['get', ['/', async (req, res) => {
   const tipOfTheDay = (await maxdome.request('tipOfTheDays').send())[0];
   const review = tipOfTheDay.review;
   const asset = review.asset;
-  let enhancedTitle;
-  switch (asset.type) {
-    case 'movie':
-      enhancedTitle = `Der Film ${asset.title}`;
-      break;
-    case 'series':
-      enhancedTitle = `Die Serie ${asset.title}`;
-      break;
-    default:
-      enhancedTitle = asset.title;
-      break;
-  }
   const maxpert = review.maxpert;
   res.send({
     uid: asset.link,
     updateDate: tipOfTheDay.published.toISOString(),
-    titleText: `Tipp des Tages: ${enhancedTitle}`,
-    mainText: `Tipp des Tages von ${maxpert.firstname} ${maxpert.surname}: ${enhancedTitle}, ${review.headline}`,
+    titleText: renderer({ asset }, ['tipOfTheDay', 'typedTitle']),
+    mainText: renderer({ asset, maxpert, review }, ['tipOfTheDay', 'maxpert', 'typedTitle', 'review']),
     redirectionUrl: asset.link,
   });
 }]];
