6ca89c55f85951504ed7979a2b9e3cb8027af7dd
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-02 16:51:43 +0200

    Some refactoring

--- src/controller.js
+++ src/controller.js
@@ -1,24 +1,23 @@
-const renderer = require('ai-renderer-maxdome');
-
-module.exports = ({ maxdome }) => ['get', ['/', async (req, res) => {
-  const tipOfTheDay = (await maxdome.request('tipOfTheDays').send())[0];
-  const review = tipOfTheDay.review;
-  if (!review) {
-    throw new Error('missing linked review in the tipOfTheDay');
-  }
-  const asset = review.asset;
-  if (!asset) {
-    throw new Error('missing linked asset in the review');
-  }
-  const maxpert = review.maxpert;
-  if (!maxpert) {
-    throw new Error('missing linked maxpert in the review');
-  }
-  res.send({
-    uid: asset.link,
-    updateDate: tipOfTheDay.published.toISOString(),
-    titleText: renderer(tipOfTheDay, ['tipOfTheDay', 'typedTitle']),
-    mainText: renderer(tipOfTheDay, ['tipOfTheDay', 'maxpert', 'typedTitle', 'review']),
-    redirectionUrl: asset.link,
-  });
-}]];
+module.exports = ({ maxdome, renderer }) =>
+  ['get', ['/', async (req, res) => {
+    const tipOfTheDay = (await maxdome.request('tipOfTheDays').send())[0];
+    const review = tipOfTheDay.review;
+    if (!review) {
+      throw new Error('missing linked review in the tipOfTheDay');
+    }
+    const asset = review.asset;
+    if (!asset) {
+      throw new Error('missing linked asset in the review');
+    }
+    const maxpert = review.maxpert;
+    if (!maxpert) {
+      throw new Error('missing linked maxpert in the review');
+    }
+    res.send({
+      uid: asset.link,
+      updateDate: tipOfTheDay.published.toISOString(),
+      titleText: renderer(tipOfTheDay, ['tipOfTheDay', 'typedTitle']),
+      mainText: renderer(tipOfTheDay, ['tipOfTheDay', 'maxpert', 'typedTitle', 'review']),
+      redirectionUrl: asset.link,
+    });
+  }]];
