ed25c06f9a8d0c22d2652f2ade8fc325569cce5b
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 16:50:53 +0200

    Remove request logging, the amazon cache destroy the idea

--- src/middlewares/logging.js
+++
@@ -1,9 +0,0 @@
-const morgan = require('morgan');
-
-module.exports = morgan('combined', {
-  stream: {
-    write: (message) => {
-      console.log(message.replace('\n', ''));
-    }
-  }
-});
--- src/web.js
+++ src/web.js
@@ -1,9 +1,7 @@
 require('dotenv-safe').config();
 
-const app = require('dexpress')();
-app.use(require('./middlewares/logging'));
 const controller = process.env.CONTROLLER || 'tipOfTheDay';
-require('dcontrollers')(app, [
+require('dcontrollers')(require('dexpress')(), [
   require(`./controllers/${controller}`)({
     maxdome: require('drequest-maxdome').getRequestBuilder(),
   }),
