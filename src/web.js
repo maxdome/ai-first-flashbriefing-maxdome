cc152ea8401b8547d10adf5529d96d8eb58ebe90
Christoph Herrmann <christoph.herrmann@maxdome.de>
2017-05-18 15:13:13 +0200

    Add dprettier

--- src/web.js
+++ src/web.js
@@ -1,11 +1,8 @@
 require('dotenv-safe').config();
 
 const controller = process.env.CONTROLLER || 'tipOfTheDay';
-require('dcontrollers')(
-  require('dexpress')(),
-  [
-    require(`./controllers/${controller}`)({
-      maxdome: require('drequest-maxdome').getRequestBuilder(),
-    }),
-  ]
-);
+require('dcontrollers')(require('dexpress')(), [
+  require(`./controllers/${controller}`)({
+    maxdome: require('drequest-maxdome').getRequestBuilder(),
+  }),
+]);
